924f005c5c4eace6b16a8904c66efc88ebe4dd6b
khusamayasir <usamayasir32@gmail.com>
2023-03-21 14:26:48 +0500

    get value by function

---
+++ assets/signup/sigunp.js
@@ -0,0 +1,9 @@
+function postVal() {
+    const username = document. getElementById('usrname'). value;
+    const email = document. getElementById('email'). value;
+    const password = document. getElementById('psw'). value;
+    const phonenumber = document. getElementById('pno'). value;
+    const gender = document. getElementById('gndr'). value;
+
+    console.log(username, password);
+}
