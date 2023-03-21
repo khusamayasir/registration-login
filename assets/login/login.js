9e67573e897bb65e93339a61bd24e2fd18aba4e3
khusamayasir <usamayasir32@gmail.com>
2023-03-21 14:20:51 +0500

    get value by function

---
+++ assets/login/login.js
@@ -0,0 +1,13 @@
+// let form = document.querySelecter('form');
+// form.addEventListener('submit', (e)=>{
+//     e.preventDefault();
+//     return false;
+// }
+// );
+
+function getVal() {
+    const username = document. getElementById('usrname'). value;
+    const password = document. getElementById('psw'). value;
+
+    console.log(username, password);
+}
