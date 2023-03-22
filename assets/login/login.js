fff7f92bf36dba022d27ad24901de9aa7d5bd863
khusamayasir <usamayasir32@gmail.com>
2023-03-22 19:22:01 +0500

    call fetch function to match data

--- assets/login/login.js
+++ assets/login/login.js
@@ -5,9 +5,16 @@
 // }
 // );
 
-function getVal() {
-    const username = document. getElementById('usrname'). value;
-    const password = document. getElementById('psw'). value;
+function postVal() {
+    const data = {
+        username : document. getElementById('usrname').value,
+        password : document. getElementById('psw').value
+    }
+    
+    fetch('/api/login',{
+        type:"POST",
+        data:JSON.stringify(data)
+    })
 
     console.log(username, password);
 }
