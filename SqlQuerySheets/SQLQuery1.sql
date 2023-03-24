05470e02567e58b3ecbca575d4b51fb5e36b975a
khusamayasir <usamayasir32@gmail.com>
2023-03-24 14:33:57 +0500

    add SQL Query Sheets

---
+++ SqlQuerySheets/SQLQuery1.sql
@@ -0,0 +1,16 @@
+CREATE TABLE usersignup(
+Id int IDENTITY(1,1) PRIMARY KEY,
+UserName varchar(100),
+Email varchar(100),
+Password varchar(100),
+PhoneNumber BIGINT NOT NULL,
+Gender varchar(15)
+);
+--
+-- DROP TABLE usersigunp
+-- DROP TABLE usersignup
+
+-- select * from usersignup
+
+INSERT INTO usersignup (Username, Email, Password, PhoneNumber)
+VALUES ('KhawajaUsamaYasir', 'usamayasir32@gmail.com', 'Usama@123', 03492402306)
