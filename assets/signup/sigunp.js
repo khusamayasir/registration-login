8dd9387890e0cdf839ded65e5967d8209c6b3065
Khawaja Usama Yasir <usamayasir32@gmail.com>
2023-03-27 03:23:55 +0500

    update signup.js

--- assets/signup/sigunp.js
+++ assets/signup/sigunp.js
@@ -1,4 +1,11 @@
-const { response } = require("express")
+var btn = document.getElementById(`btnMeS`);
+
+console.log(`this is my btn element from login js file`, btn)
+
+btn.addEventListener('click', event => {
+    event.preventDefault()
+    postVal();
+  });
 
 function postVal() {
     const data = {
@@ -8,18 +15,18 @@ function postVal() {
      phonenumber: document. getElementById('pno').value,
      gender: document. getElementById('gndr').value
     }
+    console.log(data, `I got this data here from post val func`)
 
     fetch('/api/signup', {
         method: "POST",
-        data: JSON.stringify(data)
-    })
-    
-    .then((res)=> {
-        console.log("Congratulations!! You've Successfully Signup.")
-    })
-    .catch(err => {
+        body: JSON.stringify(data),
+        headers: new Headers({'content-type': 'application/json'}),
+        //data: JSON.stringify(data)
+    }).then(res => {
+        // console.log("Congratulations!! You've Successfully Login.")
+        console.log(res)
+    }).catch(err => {
         console.log(err)
     })
-
     //console.log(username, email, password, phonenumber, gender);
 }
