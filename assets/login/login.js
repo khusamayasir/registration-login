0434874e8a834b26fbf4b7910a55dbd4f5295d41
Khawaja Usama Yasir <usamayasir32@gmail.com>
2023-03-26 23:16:04 +0500

    update login.js

--- assets/login/login.js
+++ assets/login/login.js
@@ -4,22 +4,32 @@
 //     return false;
 // }
 // );
+var btn = document.getElementById(`btnMe`);
+
+console.log(`this is my btn element from login js file`, btn)
+
+btn.addEventListener('click', event => {
+    event.preventDefault()
+    postVal();
+  });
 
 function postVal() {
     const data = {
         username: document.getElementById('usrname').value,
         password: document.getElementById('psw').value
     }
+    console.log(data, `I got this data here from post val func`)
 
     fetch('/api/login', {
-        type: "POST",
-        data: JSON.stringify(data)
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: new Headers({'content-type': 'application/json'}),
+        //data: JSON.stringify(data)
     }).then(res => {
-        console.log("Congratulations!! You've Successfully Login.")
+        // console.log("Congratulations!! You've Successfully Login.")
+        console.log(res)
     }).catch(err => {
         console.log(err)
     })
-
-
     //console.log(username, password);
 }
