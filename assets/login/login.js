7ff00500d781b02917587bb4efb44360bf778ee9
khusamayasir <usamayasir32@gmail.com>
2023-04-17 16:22:25 +0500

    update login.js

--- assets/login/login.js
+++ assets/login/login.js
@@ -13,25 +13,67 @@ var btn = document.getElementById(`btnMe`);
 
 console.log(`this is my btn element from login js file`, btn)
 
+// btn.addEventListener('click', event => {
+//     event.preventDefault()
+//     getVal('/api/login')
+//     .then(result => 
+//         {
+//             sessionStorage.setItem("token", result.access_token);
+//             const Token = sessionStorage.getItem("token");
+//             console.log("Generated Token is: ",Token)
+
+//             if(Token != "" || Token != undefined)
+//             {
+//                 console.log(result)
+//                 window.location.href = "http://localhost:3000/home.html";
+//             }
+//             else 
+//             {
+//                 alert("Sorry Incorrect Username & Password");
+//             }
+
+//             // if (result["recordset"][0][''] === 1){
+//             //     window.location.href = "home.html";
+//             //     console.log(result)
+//             // }
+//             // else if (result["recordset"][0][''] === 0 || result["recordset"][0][''] === null) 
+//             // {
+//             //     alert("Sorry Incorrect Username & Password");
+//             // }
+
+
+//         })
+//     .catch(error => 
+//         {
+//             console.log(error)
+//         });
+//   });
 btn.addEventListener('click', event => {
-    event.preventDefault()
+    event.preventDefault();
     getVal('/api/login')
-    .then(result => 
+      .then(result => {
+        sessionStorage.setItem("token", result.access_token);
+        const Token = sessionStorage.getItem("token");
+        console.log("Generated Token is: ", Token);
+  
+        if (Token)
         {
-            if (result.count.length === 1){
-                window.location.href = "home.html";
-                console.log(result)
-            }
-            else if (result.count.length === 0 || result.count.length === null) 
-            {
-                alert("Sorry Incorrect Username & Password");
-            }
-        })
-    .catch(error => 
+          alert(ifrun)
+          console.log(result);
+          window.location.href = "/home.html";
+        }
+        else
+        alert("Sorry, incorrect username and password.");
         {
-            console.log(error)
-        });
+
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
   });
+  
+  
 
 function getVal(url) {
     return new Promise((resolve, reject) => {
@@ -52,6 +94,9 @@ function getVal(url) {
         // {
         //   throw new Error('Network response was not ok');
         // }
+        //console.log(res)
+        //sessionStorage.setItem("lastname", );
+        //sessionStorage.getItem("lastname");
         resolve (res.json())
         
     // }).then(result => { 
