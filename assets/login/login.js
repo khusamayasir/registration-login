126e6ae28c8ef55c2f6bdf2d0a46f456828fe49a
khusamayasir <usamayasir32@gmail.com>
2023-04-19 15:18:04 +0500

    update login.js

--- assets/login/login.js
+++ assets/login/login.js
@@ -1,53 +1,7 @@
-// let form = document.querySelecter('form');
-// form.addEventListener('submit', (e)=>{
-//     e.preventDefault();
-//     return false;
-// }
-
-//const { json } = require("body-parser");
-
-//const { TokenExpiredError } = require("jsonwebtoken");
-
-// );
 var btn = document.getElementById(`btnMe`);
 
 console.log(`this is my btn element from login js file`, btn)
 
-// btn.addEventListener('click', event => {
-//     event.preventDefault()
-//     getVal('/api/login')
-//     .then(result => 
-//         {
-//             sessionStorage.setItem("token", result.access_token);
-//             const Token = sessionStorage.getItem("token");
-//             console.log("Generated Token is: ",Token)
-
-//             if(Token != "" || Token != undefined)
-//             {
-//                 console.log(result)
-//                 window.location.href = "http://localhost:3000/home.html";
-//             }
-//             else 
-//             {
-//                 alert("Sorry Incorrect Username & Password");
-//             }
-
-//             // if (result["recordset"][0][''] === 1){
-//             //     window.location.href = "home.html";
-//             //     console.log(result)
-//             // }
-//             // else if (result["recordset"][0][''] === 0 || result["recordset"][0][''] === null) 
-//             // {
-//             //     alert("Sorry Incorrect Username & Password");
-//             // }
-
-
-//         })
-//     .catch(error => 
-//         {
-//             console.log(error)
-//         });
-//   });
 btn.addEventListener('click', event => {
     event.preventDefault();
     getVal('/api/login')
@@ -84,27 +38,17 @@ function getVal(url) {
         method: "POST",
         body: JSON.stringify(data),
         headers: new Headers({'content-type': 'application/json'}),
-        //data: JSON.stringify(data)
     })
     .then(res => {
-        // if (!response.ok)
-        // {
-        //   throw new Error('Network response was not ok');
-        // }
-        //console.log(res)
-        //sessionStorage.setItem("lastname", );
-        //sessionStorage.getItem("lastname");
         resolve (res.json())
-        
-    // }).then(result => { 
+    })
+    //.then(result => { 
     //     resolve(result)
-        
     //     console.log('Data is: ', result)
-    // })
-    }).catch(error => {
+    //})
+    .catch(error => {
         console.log('ERROR!!')
         reject(error)
     })
 });
-    //console.log(username, password);
 }
