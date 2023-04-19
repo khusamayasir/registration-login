87b2ac919b9768bf353c0d122e9ce261a52b7587
khusamayasir <usamayasir32@gmail.com>
2023-04-19 13:15:35 +0500

    update home.js

--- assets/home/home.js
+++ assets/home/home.js
@@ -4,4 +4,13 @@ console.log(`this is my btn element from login js file`, btn)
 
 btn.addEventListener('click', event => {
     event.preventDefault();
+    function getVal() {
+      console.log("HEY USAMA")
+      let Token = sessionStorage.getItem("token")
+      //sessionStorage.removeItem(Token)
+      console.log(Token)
+      window.location.href = "/home.html";
+    }
+
+    getVal();
   });
