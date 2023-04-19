91b0dfd891e937cfcc1af8ae79b822e75f5c7b8a
khusamayasir <usamayasir32@gmail.com>
2023-04-19 15:29:12 +0500

    update home.js

--- assets/home/home.js
+++ assets/home/home.js
@@ -4,15 +4,10 @@ console.log(`this is my btn element from login js file`, btn)
 
 btn.addEventListener('click', function() {
   console.log("Hey Usama Yasir")
-  let Token = sessionStorage.getItem("token")
-  //sessionStorage.removeItem(Token)
-  console.log(Token)
-  window.location.href = "/home.html";
+  // let Token = sessionStorage.getItem("token")
+  // console.log(Token)
+  sessionStorage.removeItem("token")
+  // let Tokenss = sessionStorage.getItem("token")
+  // console.log(Tokenss)
+  window.location.href = "/login.html";
 });
-
-  // const btn = document.getElementById("clickMe");
-  // btn.addEventListener("click", function() {
-  //   let Token = sessionStorage.getItem("token");
-  //   console.log(Token);
-  //   window.location.href = "/home.html";
-  // });r
